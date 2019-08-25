1ac958589d8ca813de7aad2dcb47fb1bcddf0de9
Wesley Costa <wesley.costa.developer@gmail.com>
2019-08-25 04:05:02 -0300

    Tela de cadastro

--- boali-school-app/src/app/app-routing.module.ts
+++ boali-school-app/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cadastrar', component: CadastrarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
