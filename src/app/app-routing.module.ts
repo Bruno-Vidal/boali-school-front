86d192a3dca091682b21bfa110d5957666855728
flaks009 <flaks009@gmail.com>
2019-08-25 00:31:05 -0300

    Bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -9,7 +10,8 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    //component: TelaLoginComponent
+    component: LoginComponent
+    
   }
 ]
 
