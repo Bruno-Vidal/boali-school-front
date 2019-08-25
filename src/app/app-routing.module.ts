8cdbe116f7930352b4eb3716ed023582e31c7a10
flaks009 <flaks009@gmail.com>
2019-08-25 05:00:56 -0300

    Login view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { IntroComponent } from './intro/intro.component';
 
 const routes: Routes = [
   {
@@ -12,8 +13,15 @@ const routes: Routes = [
     path: 'login',
     component: LoginComponent
     
+  },
+
+  {
+    path: 'intro',
+    component: IntroComponent
   }
 ]
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
